0e1017df306bf70b0eafd22b39f2a058db137eb4
ZyanAlkanza <zyan.mujaddid@gmail.com>
2024-06-15 15:19:59 +0700

    membuat fungsi login

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,6 +2,16 @@
 import searchbar from './searchbar.vue';
 
 export default {
+    data() {
+        return {
+            username: '',
+            token: '',
+        }
+    },
+    mounted() {
+        this.username = localStorage.getItem('username');
+        this.token = localStorage.getItem('token');
+    },
     components: {
         searchbar
     },
@@ -15,10 +25,25 @@ export default {
         </router-link>
         <div class="button flex justify-center items-center gap-x-4">
             <searchbar />
-            <router-link to="/login"
+            <router-link to="/login" v-if="token == null"
                 class="px-6 py-2 text-white bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out rounded">
                 <i class="ri-login-box-line mr-1"></i>Masuk
             </router-link>
+
+            <!-- <router-link to="/logout" v-if="token != null"
+                class="px-6 py-2 text-white bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out rounded">
+                <i class="ri-logout-box-line mr-1"></i>Logout
+            </router-link> -->
+
+            <details class="dropdown" v-if="token != null">
+                <summary class="m-1 btn bg-[#eaeaea]">Hi, {{ username }}</summary>
+                <ul class="p-2 shadow menu dropdown-content z-[1] bg-gray-100 rounded-box w-52">
+                    <li><router-link to="/profile"><i class="ri-user-6-line mr-1"></i>Profil</router-link></li>
+                    <li><router-link to="/profile"><i class="ri-book-open-line mr-1"></i>Buku Saya</router-link></li>
+                    <li><router-link to="/mark"><i class="ri-bookmark-line mr-1"></i>Koleksi</router-link></li>
+                    <li><router-link to="/logout"><i class="ri-logout-box-line mr-1"></i>Keluar</router-link></li>
+                </ul>
+            </details>
         </div>
     </main>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,13 @@
 <template>
     <main class="h-full flex flex-col gap-y-4 justify-start items-center">
         <div class="alert w-1/4 h-20 mt-20 flex">
-            <span class="w-full p-4 mt-6 text-red-500 text-sm font-semibold bg-red-100 rounded">
+            <span v-if="errors" class="w-full p-4 mt-6 text-red-500 text-sm font-semibold bg-red-100 rounded">
                 <i class="ri-close-large-line mr-2"></i>
-                Pesan kesalahan
+                {{ errors.message }}
+            </span>
+            <span v-if="alert" class="w-full p-4 mt-6 text-green-500 text-sm font-semibold bg-green-100 rounded">
+                <i class="ri-check-line mr-2"></i>
+                {{ alert }}
             </span>
         </div>
         <form @submit.prevent="login" class="card w-1/4 px-8 py-4 bg-gray-100 rounded">
@@ -13,7 +17,8 @@
                 <input type="email" v-model="email" name="email" id="email"
                     class="px-2 py-1 bg-white focus:outline-none border-2 border-white focus:border-blue-500 rounded">
                 <div class="validation h-6 flex justify-start">
-                    <small class="text-red-500 leading-none font-semibold">Pesan kesalahan</small>
+                    <small v-if="validations.email" class="text-red-500 leading-none font-semibold">{{
+                        validations.email[0] }}</small>
                 </div>
             </div>
             <div class="password mb-6 flex flex-col gap-y-2">
@@ -21,7 +26,8 @@
                 <input type="password" v-model="password" name="password" id="password"
                     class="px-2 py-1 bg-white focus:outline-none border-2 border-white focus:border-blue-500 rounded">
                 <div class="validation h-6 flex justify-start">
-                    <small class="text-red-500 leading-none font-semibold">Pesan kesalahan</small>
+                    <small v-if="validations.password" class="text-red-500 leading-none font-semibold">{{
+                        validations.password[0] }}</small>
                 </div>
             </div>
             <button type="submit"
@@ -35,3 +41,55 @@
         </form>
     </main>
 </template>
+
+<script>
+import router from '@/router'
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            alert: '',
+            errors: '',
+            validations: '',
+            token: '',
+        }
+    },
+    methods: {
+        login() {
+            axios.post('http://127.0.0.1:8000/api/login', {
+                email: this.email,
+                password: this.password
+            })
+                .then(response => {
+                    localStorage.setItem('token', response.data.token);
+                    localStorage.setItem('username', response.data.data.username);
+                    localStorage.setItem('role', response.data.data.role);
+                    this.alert = response.data.message;
+                    if (response.data.data.role == 1) {
+                        setTimeout(() => {
+                            router.push('/')
+                        }, 1000);
+                    } else if (response.data.data.role == 3) {
+                        setTimeout(() => {
+                            router.push('/')
+                        }, 1000);
+                    }
+                })
+                .catch(error => {
+                    this.errors = error.response.data;
+                    this.validations = error.response.data.data;
+                });
+        }
+
+    },
+    mounted() {
+        this.token = localStorage.getItem('token');
+        if (this.token) {
+            router.push('/');
+        }
+    },
+}
+</script>
