d6fa3422c0a71b9364d99ae15bb17d8efff1a671
ZyanAlkanza <zyan.mujaddid@gmail.com>
2024-07-13 21:20:55 +0700

    membuat fungsi menu mark

--- src/views/MarkView.vue
+++ src/views/MarkView.vue
@@ -3,19 +3,21 @@
     <main class="h-5/6">
 
         <section class="px-20 py-10 grid grid-cols-8 gap-6">
-            <router-link :to="`/detail`"
+            <router-link :to="`/detail/${mark.book.id}`" v-for="mark in marks"
                 class="h-max px-2 pt-2 pb-4 flex flex-col bg-gray-100 rounded hover:shadow-md transition duration-300 ease-in-out">
                 <div class="image h-[75%] flex justify-center rounded bg-gray-100">
-                    <img src="/default_cover.png" class="h-full rounded" alt="cover_buku">
+                    <img :src="mark.book.image ? url + mark.book.image : '/default_cover.png'" class="h-full rounded"
+                        alt="cover_buku">
                 </div>
                 <div class="text">
-                    <h5 class="w-max px-2 py-1 my-2 text-blue-500 text-xs font-semibold rounded bg-blue-100">Tersedia
-                    </h5>
-                    <!-- <h5 v-if="book.status == 2"
+                    <h5 v-if="mark.book.status == 1"
+                        class="w-max px-2 py-1 my-2 text-blue-500 text-xs font-semibold rounded bg-blue-100">
+                        Tidak Tersedia</h5>
+                    <h5 v-if="mark.book.status == 2"
                         class="w-max px-2 py-1 my-2 text-red-500 text-xs font-semibold rounded bg-red-100">
-                        Tidak Tersedia</h5> -->
-                    <h3 class="text-gray-500 text-xs font-medium">Penulis</h3>
-                    <h1 class="font-semibold truncate">Judul Buku</h1>
+                        Tidak Tersedia</h5>
+                    <h3 class="text-gray-500 text-xs font-medium">{{ mark.book.author }}</h3>
+                    <h1 class="font-semibold truncate">{{ mark.book.title }}</h1>
                 </div>
             </router-link>
         </section>
@@ -25,10 +27,36 @@
 
 <script>
 import navigation from '@/components/navigation.vue';
+import axios from 'axios';
 
 export default {
     components: {
         navigation
-    }
+    },
+    data() {
+        return {
+            marks: [],
+            id: localStorage.getItem('id'),
+            url: `http://127.0.0.1:8000/storage/covers/`,
+        }
+    },
+    methods: {
+        fetchMarks() {
+            axios.get(`http://127.0.0.1:8000/api/marks/${this.id}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then(response => {
+                    this.marks = response.data.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        }
+    },
+    mounted() {
+        this.fetchMarks();
+    },
 }
 </script>
