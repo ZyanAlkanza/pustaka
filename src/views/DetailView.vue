687a53bdcd3119d442f61e5293d5a104d74f2cf1
ZyanAlkanza <zyan.mujaddid@gmail.com>
2024-07-14 15:21:05 +0700

    membuat fungsi addMark

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -17,12 +17,17 @@
                     class="w-5/6 h-[360px] resize-none focus:outline-none cursor-default">{{ book.book_detail }}
                 </textarea>
                 <div class="button mt-3 flex gap-4">
-                    <button
+                    <button @click="addMark()"
                         class="px-4 py-2 text-blue-500 hover:text-white hover:bg-blue-600 border-2 border-blue-500 hover:border-blue-600 rounded transition duration-300 ease-in-out"><i
                             class="ri-bookmark-line mr-1"></i>Koleksi</button>
                     <button
                         class="px-8 py-2 text-white bg-blue-500 hover:bg-blue-600 border-2 border-blue-500 hover:border-blue-600 rounded transition duration-300 ease-in-out"><i
                             class="ri-book-open-line mr-1"></i>Pinjam Buku</button>
+                </div>
+            </div>
+            <div v-if="successToast" class="fixed right-10 bottom-10">
+                <div class="px-4 py-4 text-white bg-blue-500 rounded">
+                    <i class="ri-bookmark-fill mr-2"></i><span>Berhasil Ditambahkan Ke Koleksi</span>
                 </div>
             </div>
         </section>
@@ -40,7 +45,10 @@ export default {
     data() {
         return {
             book: '',
-            url: 'http://127.0.0.1:8000/storage/covers/'
+            url: 'http://127.0.0.1:8000/storage/covers/',
+            user_id: localStorage.getItem('id'),
+            book_id: this.$route.params.id,
+            successToast: false
         }
     },
     methods: {
@@ -51,6 +59,26 @@ export default {
                 })
                 .catch(error => {
                     console.error(error);
+                })
+        },
+        addMark() {
+            axios.post(`http://127.0.0.1:8000/api/addMark`, {
+                user_id: this.user_id,
+                book_id: this.book_id,
+            }, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then(response => {
+                    this.successToast = true
+                    setTimeout(() => {
+                        this.successToast = false
+                    }, 1500);
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error(error.response.data.message);
                 })
         }
     },
